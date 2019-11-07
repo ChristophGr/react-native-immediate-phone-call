a81ff2f2fc2032fa163cf8b3e1eea8adf23008da
Christoph Gritschenberger <christoph.gritschenberger@cca.io>
2019-11-07 11:25:02 +0100

    Revert "Merge pull request #19 from kukadiajayesh/permission-patch"
    This reverts commit 97066b8ab95e6118b1bcd40bf70a4e8146f47150, reversing
    changes made to f8bfdc9b6f04499a5c9398495b6995ef43a0219c.

--- android/src/main/java/com/github/wumke/RNImmediatePhoneCall/RNImmediatePhoneCallModule.java
+++ android/src/main/java/com/github/wumke/RNImmediatePhoneCall/RNImmediatePhoneCallModule.java
@@ -1,31 +1,29 @@
 package com.github.wumke.RNImmediatePhoneCall;
 
-import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-
+import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class RNImmediatePhoneCallModule extends ReactContextBaseJavaModule {
 
-    private static RNImmediatePhoneCallModule rnImmediatePhoneCallModule;
-
-    private ReactApplicationContext reactContext;
-    private static String number = "";
-    private static final int PERMISSIONS_REQUEST_ACCESS_CALL = 101;
+    ReactApplicationContext reactContext;
+    AlarmManager alarmManager;
 
     public RNImmediatePhoneCallModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        if (rnImmediatePhoneCallModule == null) {
-            rnImmediatePhoneCallModule = this;
-        }
-        rnImmediatePhoneCallModule.reactContext = reactContext;
+        this.reactContext = reactContext;
+        alarmManager = (AlarmManager) reactContext.getSystemService(Context.ALARM_SERVICE);
     }
 
     @Override
@@ -35,36 +33,10 @@ public class RNImmediatePhoneCallModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void immediatePhoneCall(String number) {
-        RNImmediatePhoneCallModule.number = Uri.encode(number);
-
-        if (ContextCompat.checkSelfPermission(reactContext.getApplicationContext(),
-                android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-            call();
-        } else {
-            ActivityCompat.requestPermissions(getCurrentActivity(),
-                    new String[]{android.Manifest.permission.CALL_PHONE},
-                    PERMISSIONS_REQUEST_ACCESS_CALL);
-        }
-    }
-	
-	@SuppressLint("MissingPermission")
-    private static void call() {
-        String url = "tel:" + RNImmediatePhoneCallModule.number;
+        number = Uri.encode(number);
+        String url = "tel:" + number;
         Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(url));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        rnImmediatePhoneCallModule.reactContext.startActivity(intent);
-    }
-
-    public static void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_CALL: {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    call();
-                }
-            }
-        }
+        this.reactContext.startActivity(intent);
     }
 }
--- android/src/main/java/com/github/wumke/RNImmediatePhoneCall/RNImmediatePhoneCallPackage.java
+++ android/src/main/java/com/github/wumke/RNImmediatePhoneCall/RNImmediatePhoneCallPackage.java
@@ -1,16 +1,15 @@
 package com.github.wumke.RNImmediatePhoneCall;
 
-import android.support.annotation.NonNull;
-
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNImmediatePhoneCallPackage implements ReactPackage {
 
@@ -27,6 +26,7 @@ public class RNImmediatePhoneCallPackage implements ReactPackage {
     // override removed to be compatible with rn0.47+
     //@Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
+
         return Collections.emptyList();
     }
 
@@ -34,11 +34,6 @@ public class RNImmediatePhoneCallPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(
             ReactApplicationContext reactContext) {
         return Collections.emptyList();
-    }
-	
-	public static void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
-                                                  @NonNull int[] grantResults) {
-        RNImmediatePhoneCallModule.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
 }
